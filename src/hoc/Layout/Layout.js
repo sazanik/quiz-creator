cfe309da60565df5e8139fd00b06a224543228d5
kingurru <kingnikoriginal@gmail.com>
2021-05-25 12:42:14 +0300

    feat: add drawer & backdrop

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from './Layout.module.css'
 import MenuToggle from "../../components/navigation/MenuToggle/MenuToggle";
+import Drawer from "../../components/navigation/Drawer/Drawer";
 
 class Layout extends React.Component {
   state = {
@@ -14,9 +15,19 @@ class Layout extends React.Component {
     })
   }
 
+  menuCloseHandler = () => {
+    this.setState({
+      menu: false
+    })
+  }
+
   render() {
     return (
       <div className={classes.Layout}>
+
+        <Drawer
+          onClose={this.menuCloseHandler}
+          isOpen={this.state.menu}/>
         <MenuToggle
           isOpen={this.state.menu}
           onToggle={this.toggleMenuHandler}
