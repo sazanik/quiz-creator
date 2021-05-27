379fa4fa9bb209e2a2500990ea9832443e15795d
kingurru <kingnikoriginal@gmail.com>
2021-05-27 08:55:42 +0300

    feat: add login & registr method with firebase

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import classes from './Auth.module.css'
 import Button from "../../components/UI/Button/Button";
 import Input from "../../components/UI/Input/Input";
 import is from 'is_js'
+import axios from "axios";
 
 
 /*function validateEmail(email) {
@@ -42,12 +43,34 @@ class Auth extends React.Component {
     }
   }
 
-  loginHandler = () => {
+  loginHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true,
+    }
 
+    try {
+      const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD0P9HAXHXtbe7cyUeeMNKyk-JNUrukId8', authData)
+      console.log(response.data)
+    } catch (error) {
+      console.log(error)
+    }
   }
 
-  registerHandler = () => {
+  registerHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true,
+    }
 
+    try {
+      const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD0P9HAXHXtbe7cyUeeMNKyk-JNUrukId8', authData)
+      console.log(response.data)
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   submitHandler = e => {
